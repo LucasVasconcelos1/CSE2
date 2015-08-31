5a29b4f3383b8a2352cda9a9a4b7f1d95c13f497
lucasvasconcelos1 <lqv219@lehigh.edu>
2015-08-31 19:24:31 +0000

    new version

---
+++ Hw01/Hw01.java
@@ -0,0 +1,12 @@
+// Lucas Vasconcelos hw01 2015
+// autobiographical summer break tweet below.
+-----------
+| WELCOME |
+-----------
+  ^  ^  ^  ^  ^  ^
+ / \/ \/ \/ \/ \/ \
+<-L--Q--V--2--1--9->
+ \ /\ /\ /\ /\ /\ /
+  v  v  v  v  v  v 
+During my last hiatus as a high school student, 
+I spent lots of time with friends and family.
