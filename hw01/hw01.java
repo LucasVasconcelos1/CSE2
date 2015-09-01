e01ca1098434c80df424aa6aae7c4062b120e453
lucasvasconcelos1 <lqv219@lehigh.edu>
2015-09-01 19:04:02 +0000

    updated hw01

---
+++ hw01/hw01.java
@@ -0,0 +1,15 @@
+// Lucas Vasconcelos hw01 2015
+// autobiographical summer break tweet below.
+public class hw01{ 
+public static void main(String[] args) {
+System.out.println("-----------");
+System.out.println("| WELCOME |");
+System.out.println("-----------");
+System.out.println("  ^  ^  ^  ^  ^  ^  "); 
+System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+System.out.println("<-L--Q--V--2--1--9->");
+System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+System.out.println("  v  v  v  v  v  v  ");  
+System.out.println("During my last hiatus as a high school student, I spent lots of time with friends and family.");
+   }
+}
