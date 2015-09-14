0d4b102df8574bca7dfbe71420c3018442b476f7
lucasvasconcelos1 <lqv219@lehigh.edu>
2015-09-14 19:54:52 +0000

    finished lab03

--- lab03/Check.java
+++ lab03/Check.java
@@ -13,25 +13,32 @@ public static void main(String[] args)  {
     Scanner myScanner = new Scanner( System.in );
 // construct the instance of Scanner which tells Scanner that 
 //you are creating an instance that will take input from STDIN
-double checkCost = myScanner.nextDouble();
+System.out.print("Enter the original cost of the check: $");
 // prompt user for the original cost of the check
-System.out.print("Enter the original cost of the check "+(checkCost)+".");
+double checkCost = myScanner.nextDouble();
 // accept user input for the total check cost and print output
-double tipPercent = myScanner.nextDouble() / 100;
+System.out.print("Enter the percentage tip you wish to pay: ");
 // prompt user for the percentage tip
-// convert the percentage into a decimal value
-System.out.print("Enter the percentage tip you wish to pay "+tipPercent+".");
+double tipPercent = myScanner.nextDouble() / 100;
 // accept user input for the tip percentage and print output
-int numPeople = myScanner.nextInt();
+System.out.print("Enter the number of people who went out to dinner: ");
 // prompt user for the number of people splitting the check
-System.out.print("Enter the number of people who went out to dinner "+numPeople+"." );
+int numPeople = myScanner.nextInt();
 // accept user input for the number of people splitting the check
 double totalCost$ = ((double) checkCost * (1 + (double) tipPercent));
 // 
-double costPerPerson = ((double) totalCost$ / (int) numPeople);
-//
-System.out.println(" Each person must pay "+costPerPerson+".");
-//
+double costPerPerson = (((double) totalCost$ / (int) numPeople) * 100);
+// declaring variable cost per person as a double and assigning a value
+//to it then deviding by 100 to cut off decimal values to the hundreth slot
+double costPerPersonFinal = ((int) costPerPerson);
+double costPerPersonFinal$ = (costPerPersonFinal / 100);
+// declaring new cost per person variable as a double and multiplying
+// the old costPerPerson by 100 to move the decimal and achieve a 
+// number that makes sense for currency and payment.
+
+System.out.println(" Each person must pay $"+costPerPersonFinal$+".");
+// print out the final output telling us how much money each person
+// must pay
     }
 // end of main method
 }
