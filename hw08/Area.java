bb0e5a73e4188e6eb1e5f531a5440716748bf851
lucasvasconcelos1 <lqv219@lehigh.edu>
2015-10-21 19:42:37 +0000

    completed hw08

---
+++ hw08/Area.java
@@ -0,0 +1,171 @@
+// Lucas Vasconcelos 10/18/15 hw08 Area
+/* This program will calculate the area of three different shapes; a rectangle,
+a triangle and a circle using multiple methods*/
+import java.util.Scanner;
+// import the scanner class
+public class Area {
+    // beginnning of class
+    public static double inputCheck(String shape){
+        Scanner myScanner = new Scanner(System.in);
+        // create the instance for using the scanner class in the inputCheck method
+        double area = 0;
+        if(shape.equals("rectangle")){
+            System.out.print("Enter a double value for the height: ");
+            // prompt the user to input a double number for a
+            double height;
+            // accept user input for a
+            while (true) {
+                if (!myScanner.hasNextDouble()) {
+                    System.out.print("Sorry, you did not enter a double number. Try again: ");
+                    myScanner.next();
+                }
+                else {
+                    height = myScanner.nextDouble();
+                    if (height < 0) {
+                        System.out.print(" Please only enter doubles greater than 0. Try again: ");
+                    }
+                    else {
+                        break;                    
+                    }
+                }
+            }
+            // use a while loop with nested if statements to prevent invalid inputs for height
+            System.out.print("Enter a double value for the base: ");
+            // prompt the user to input a double number for b
+            double base;
+            // accept user input for base
+            while (true) {
+                if (!myScanner.hasNextDouble()) {
+                    System.out.print("Sorry, you did not enter a double number. Try again: ");
+                    myScanner.next();
+                }
+                else {
+                    base = myScanner.nextDouble();
+                    if (base < 0) {
+                        System.out.print(" Please only enter doubles greater than 0. Try again: ");
+                    }
+                    else {
+                        break;                    
+                    }
+                }
+            }
+            // use a while loop with nested if statements to prevent invalid inputs for base
+            area = rectangleA(height, base);
+        }
+        else if(shape.equals("triangle")) {
+            System.out.print("Enter a double value for the height: ");
+            // prompt the user to input a double number for a
+            double height;
+            // accept user input for a
+            while (true) {
+                if (!myScanner.hasNextDouble()) {
+                    System.out.print("Sorry, you did not enter a double number. Try again: ");
+                    myScanner.next();
+                }
+                else {
+                    height = myScanner.nextDouble();
+                    if (height < 0) {
+                        System.out.print(" Please only enter doubles greater than 0. Try again: ");
+                    }
+                    else {
+                        break;                    
+                    }
+                }
+            }
+            // use a while loop with nested if statements to prevent invalid inputs for height
+            System.out.print("Enter a double value for the base: ");
+            // prompt the user to input a double number for b
+            double base;
+            // accept user input for base
+            while (true) {
+                if (!myScanner.hasNextDouble()) {
+                    System.out.print("Sorry, you did not enter a double number. Try again: ");
+                    myScanner.next();
+                }
+                else {
+                    base = myScanner.nextDouble();
+                    if (base < 0) {
+                        System.out.print(" Please only enter doubles greater than 0. Try again: ");
+                    }
+                    else {
+                        break;                    
+                    }
+                }
+            }
+            // use a while loop with nested if statements to prevent invalid inputs for base
+            area = triangleA(height, base);
+        }
+        else if(shape.equals("circle")) {
+            System.out.print("Enter a double value for the radius: ");
+            // prompt the user to input a double number for a
+            double radius;
+            // accept user input for a
+            while (true) {
+                if (!myScanner.hasNextDouble()) {
+                    System.out.print("Sorry, you did not enter a double number. Try again: ");
+                    myScanner.next();
+                }
+                else {
+                    radius = myScanner.nextDouble();
+                    if (radius < 0) {
+                        System.out.print(" Please only enter doubles greater than 0. Try again: ");
+                    }
+                    else {
+                        break;                    
+                    }
+                }
+            }
+            area = circleA(radius);
+        }
+        return area;
+    }
+    //create input check method that will calculate area and check inputs 
+    public static double rectangleA(double a, double b) {
+        return a*b;
+    }
+    public static double circleA(double c) {
+        return (3.14*(c*c));
+    }
+    public static double triangleA(double e, double f) {
+        return (e*f)/2;
+    }
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        // create the instance for using the scanner class in the main method
+        System.out.print("What type of shape would you like to choose: ");
+        // prompt the user to pick a shape
+        String shape;
+        // accept user input for the type of shape
+        while (true) {
+            if (!myScanner.hasNextLine()) {
+                System.out.print("Sorry, you did not enter a String variable. Try again: ");
+                myScanner.next();
+            }
+            else {
+                shape = myScanner.nextLine();
+                if (!shape.equals("rectangle") && !shape.equals("circle") && !shape.equals("triangle")) {
+                    System.out.print(" You must choose among a rectangle, a square, and a triangle. Try again: ");
+                }
+                else {
+                    break;                    
+                }
+            }
+        }
+        // use a while loop with nested if statements to prevent invalid inputs
+        if (shape.equals("rectangle")) {
+        double area = inputCheck(shape);
+        System.out.println("The area of the rectangle is "+area+".");
+        }
+        //print out area of rectangle if shape is equal to rectangle
+        else if (shape.equals("circle")) {
+        double area = inputCheck(shape);
+        System.out.println("The area of the square is "+area+".");
+        }
+        //print out the area of a circle if shape is equal to circle
+        else if (shape.equals("triangle")) {
+        double area = inputCheck(shape);
+        System.out.println("The area of the triangle is "+area+".");
+        }
+        //print out the area of a triangle if shape is equal to triangle
+    } //end of main method
+} //end of public class Area
---
+++ hw08/StringAnalysis.java
@@ -0,0 +1,138 @@
+// Lucas Vasconcelos 10/20/15 hw08 StringAnalysis
+/* This program will process a string by examining all the characters, or just 
+a specified number of characters in the string, and determining if they are 
+letters. This program will also let the user enter a string and choose if he or
+she wants to examine all the characters or just a certain number*/
+import java.util.Scanner;
+// import the scanner class
+public class StringAnalysis {
+    // beginnning of class
+    
+    public static boolean main2( String a) {
+        //beginning of the first method
+        boolean t = true;
+        int length = a.length();
+        //determine the length of the string
+        int counter = 0;
+        /*create counter variable that will stop the string checking process when
+        the string ends*/
+        while (counter < length) {
+            if (!Character.isLetter(a.charAt(counter))) {
+                t = false;
+                break;
+            }
+            counter++;
+        }
+        //use while loop with a nested if statement to check the string
+        if (counter==length)  {
+        t = true;
+        }
+        /*use if statement to assign true to t if the string has no numbers or
+        characters other than letters*/
+        return t;
+        //return boolean value stored in t
+    }
+    public static boolean main2( String b, int c) {
+        //beginning of second method
+        boolean t = true;
+        Scanner myScanner = new Scanner(System.in);
+        // create the instance for using the scanner class in the main method
+        int length = b.length();
+        //determine the length of the string
+        int counter = 0;
+        while (counter < c && counter < length) {
+            if (Character.isLetter(b.charAt(counter))) {
+                counter++;
+            }
+            else {
+                t = false;
+            }
+        }
+        //use while loop with a nested if statement to check the string
+        if (counter==c) {
+            t = true;
+        }
+        //use if statement to assign t the value true if the string only contains letters
+        return t;
+        //return boolean value stored in t
+    }
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        // create the instance for using the scanner class in the main method
+        System.out.print("Enter a string: ");
+        // prompt the user to enter a string
+        String string;
+        // accept user input for a string
+        while (true) {
+            if (!myScanner.hasNextLine()) {
+                System.out.print("Sorry, you did not enter a String variable. Try again: ");
+                myScanner.nextLine();
+            }
+            else {
+                string = myScanner.nextLine();
+                break;                    
+            }
+        }
+        // use a while loop with nested if statements to prevent invalid inputs
+        boolean allLetters1 = main2(string);
+        if (allLetters1==true) {
+            System.out.println("All of the characters in the string are letters.");
+        }
+        else {
+            System.out.println("Not all of the characters in the string are letters.");
+        }
+        System.out.print("Enter the number of characters you want to check: ");
+        // prompt the user to enter the number of characters to be checked
+        int d;
+        // accept user input for the number of characters to be checked
+        while (true) {
+            if (!myScanner.hasNextInt()) {
+                System.out.print("Sorry, you did not enter an integer variable. Try again: ");
+                myScanner.next();
+            }
+            else {
+                d = myScanner.nextInt();
+                if (d < 0) {
+                    System.out.print(" You must enter a number greater than 0. Try again: ");
+                }
+                else {
+                    break;                    
+                }
+            }
+        }
+        // use a while loop with nested if statements to prevent invalid inputs
+        boolean allLetters2 = main2(string,d);
+        if (allLetters2==true) {
+            System.out.println("All of the characters in this segment are letters.");
+        }
+        else {
+            System.out.println("Not all of the characters in this segment are letters.");
+        }
+        //print out the output depending on whether or not all letters were entered
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
